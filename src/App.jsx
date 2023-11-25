52f01725e305413a1d12460c090210f4d2b6c883
Juan Ceballos <juandavi789@hotmail.com>
2023-11-25 00:19:44 -0500

    Add initial screen structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='top-img'>
+        <img src='blobslemon.png' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <main className='main-content'>
+        <div className='content-container'>
+          <h1 className='title'>Quizzical</h1>
+          <p className='game-desc'>A funny question app</p>
+          <button className='cta cta-start'>Star quiz</button>
+        </div>
+      </main>
+      <div className='bottom-img'>
+        <img src='blobsbaby.png' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
