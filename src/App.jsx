520087779dbbde0fe7d1f272c1db48fc56fef6d3
Juan Ceballos <juandavi789@hotmail.com>
2023-11-26 00:08:22 -0500

    Create state for check when the user has started the game, for keeping track of the fetched question from the open trivia API, also conditionally render content base on the user selection of the gameStatus (started or not started)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [gameStatus, setGameStatus] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (gameStatus) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://opentdb.com/api.php?amount=5');
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const jsonData = await response.json();
+          setData(jsonData.results);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [gameStatus]);
+
   return (
     <div className='container'>
       <div className='top-img'>
@@ -8,9 +31,20 @@ const App = () => {
       </div>
       <main className='main-content'>
         <div className='content-container'>
-          <h1 className='title'>Quizzical</h1>
-          <p className='game-desc'>A funny question app</p>
-          <button className='cta cta-start'>Star quiz</button>
+          {gameStatus ? (
+            <h1>melo</h1>
+          ) : (
+            <>
+              <h1 className='title'>Quizzical</h1>
+              <p className='game-desc'>A funny question app</p>
+            </>
+          )}
+          <button
+            className='cta cta-start'
+            onClick={() => setGameStatus((prevGameStatus) => !prevGameStatus)}
+          >
+            Star quiz
+          </button>
         </div>
       </main>
       <div className='bottom-img'>
